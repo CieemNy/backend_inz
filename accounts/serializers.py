27f737d0ff7454c6f2482684685df1728b763cab
Damian Banach <damian070799@wp.pl>
2022-11-12 13:58:15 +0100

    created endpoint to display users list

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -7,4 +7,10 @@ User = get_user_model()
 class UserCreateSerializer(UserCreateSerializer):
     class Meta(UserCreateSerializer.Meta):
         model = User
-        fields = ('id', 'email', 'name', 'surname', 'password')
+        fields = [
+            'id',
+            'email',
+            'name',
+            'surname',
+            'password'
+        ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .serializers import *
+from rest_framework import generics, permissions, status
 
-# Create your views here.
+User = get_user_model()
+
+
+# endpoint: display list of users
+
+class UserList(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserCreateSerializer
+    name = 'users-list'
