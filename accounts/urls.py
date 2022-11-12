234fe29a0da8a89339576af5e34bffcecce1c287
Damian Banach <damian070799@wp.pl>
2022-11-12 14:12:19 +0100

    updated urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from . import views
+from django.contrib import admin
 
+admin.site.site_url = 'http://127.0.0.1:8000/accounts/'
 urlpatterns = [
     path('users', views.UserList.as_view(), name='user-list'),
     path('users/<int:pk>', views.UserDetail.as_view(), name='user-detail'),
