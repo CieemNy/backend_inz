fe1c802a10b617abfb8dc49e81e6097ff1c2a4eb
Damian Banach <damian070799@wp.pl>
2022-11-12 14:00:17 +0100

    created endpoint to display user detail

--- accounts/views.py
+++ accounts/views.py
@@ -11,3 +11,16 @@ class UserList(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserCreateSerializer
     name = 'users-list'
+
+
+# endpoint: display, edit, delete user details
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserCreateSerializer
+    name = 'user-detail'
+
+    def perform_update(self, serializer):
+        instance = serializer.save()
+        instance.set_password(instance.password)
+        instance.save()
