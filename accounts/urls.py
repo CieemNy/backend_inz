23e828863812eb36a2d1f67c570092badeeb8917
Damian Banach <damian070799@wp.pl>
2022-11-15 17:23:27 +0100

    create path to create company

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,5 @@ admin.site.site_url = 'http://127.0.0.1:8000/accounts/'
 urlpatterns = [
     path('users', views.UserList.as_view(), name='user-list'),
     path('users/<int:pk>', views.UserDetail.as_view(), name='user-detail'),
+    path('company', views.CreateCompany.as_view(), name='create-company'),
 ]
