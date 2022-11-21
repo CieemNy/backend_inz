79fb0a19096b267d73b2c2bb5dfc0da69e83cd02
Damian Banach <damian070799@wp.pl>
2022-11-21 16:51:34 +0100

    added Team to admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
-from .models import UserAccount, Company
+from .models import UserAccount, Company, Team
 
 
 class UserAccountAdmin(admin.ModelAdmin):
-    list_display = ['id', 'is_superuser', 'email', 'name', 'surname', 'is_active', 'is_staff', 'is_verified', 'is_company', 'is_leader', 'is_member']
+    list_display = ['id', 'is_superuser', 'is_staff', 'email', 'name', 'surname', 'membership', 'is_active', 'is_verified', 'is_company', 'is_leader', 'is_member']
     list_editable = ['is_verified', 'is_company', 'is_leader', 'is_member']
 
 
@@ -12,5 +12,10 @@ class CompanyAdmin(admin.ModelAdmin):
     list_display = ['id', 'user', 'name', 'description', 'contact_number', 'contact_email', 'main_front', 'main_back', 'occupied_places', 'places']
 
 
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'name', 'occupied_places', 'places', 'creation_date', 'completion_date']
+
+
 admin.site.register(UserAccount, UserAccountAdmin)
 admin.site.register(Company, CompanyAdmin)
+admin.site.register(Team, TeamAdmin)
