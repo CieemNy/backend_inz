f8cbcd7b3c0d016d77fa2785f5175c943f790376
Damian Banach <damian070799@wp.pl>
2022-12-10 13:08:10 +0100

    added teamchoices to admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import UserAccount, Company, Team, Members, Project
+from .models import UserAccount, Company, Team, Members, Project, TeamChoices
 
 
 class UserAccountAdmin(admin.ModelAdmin):
@@ -24,8 +24,13 @@ class ProjectAdmin(admin.ModelAdmin):
     list_display = ['id', 'company', 'title', 'description', 'front', 'back']
 
 
+class TeamChoicesAdmin(admin.ModelAdmin):
+    list_display = ['id', 'choice_first', 'choice_second', 'choice_third', 'choice_fourth', 'final_choice', 'choice_first', 'is_considered', 'date']
+
+
 admin.site.register(UserAccount, UserAccountAdmin)
 admin.site.register(Company, CompanyAdmin)
 admin.site.register(Team, TeamAdmin)
 admin.site.register(Members, MembersAdmin)
 admin.site.register(Project, ProjectAdmin)
+admin.site.register(TeamChoices, TeamChoicesAdmin)
